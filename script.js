b555b82739f7126423c6a59e193da0e689e61007
Exi <eximo888@gmail.com>
2023-04-27 19:14:41 +0200

    now shows validation on password fields

--- script.js
+++ script.js
@@ -7,18 +7,33 @@ const form = document.querySelector("form");
 // console.log(pwd.value);
 // console.log(confirmPwd.value);
 
+pwd.addEventListener("input", (event) => {
+  if (!pwd.checkValidity()) {
+    pwd.classList = "error";
+  } else {
+    pwd.classList = "";
+  }
+});
+confirmPwd.addEventListener("input", (event) => {
+  if (!confirmPwd.checkValidity()) {
+    confirmPwd.classList = "error";
+  } else {
+    confirmPwd.classList = "";
+  }
+});
 form.addEventListener("submit", (event) => {
-//   console.log(pwd.value);
-//   console.log(confirmPwd.value);
+  //   console.log(pwd.value);
+  //   console.log(confirmPwd.value);
   if (pwd.value !== confirmPwd.value) {
     // console.log("not same");
     pwdError.textContent = "*Password do not match";
+    pwd.classList = "error";
+    confirmPwd.classList = "error";
     event.preventDefault();
   } else {
     pwdError.textContent = "";
     // console.log("same");
   }
-  
 
   //   if (pwd.textContent !== confirmPwd.textContent) {
   //     pwdError.textContent = "Password do not match";
