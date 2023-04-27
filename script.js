2b5585f3192395951068bde64d3ce65f0e8f56ef
Exi <eximo888@gmail.com>
2023-04-27 16:32:28 +0200

    impoved mobile, added JS validation for passwords to match, styling fixes

---
+++ script.js
@@ -0,0 +1,26 @@
+const pwd = document.querySelector("#first-pwd");
+const confirmPwd = document.querySelector("#confirm-pwd");
+const pwdError = document.querySelector("#error");
+const form = document.querySelector("form");
+
+console.log(form);
+// console.log(pwd.value);
+// console.log(confirmPwd.value);
+
+form.addEventListener("submit", (event) => {
+  console.log(pwd.value);
+  console.log(confirmPwd.value);
+  if (pwd.value !== confirmPwd.value) {
+    console.log("not same");
+    pwdError.textContent = "*Password do not match";
+  } else {
+    pwdError.textContent = "";
+    console.log("same");
+  }
+  event.preventDefault();
+
+  //   if (pwd.textContent !== confirmPwd.textContent) {
+  //     pwdError.textContent = "Password do not match";
+  //     event.preventDefault();
+  //   }
+});
